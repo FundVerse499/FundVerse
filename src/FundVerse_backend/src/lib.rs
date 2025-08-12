d263c3f4388a8c2d70abb2ef9c58004038ff5ccf
MarwanIssa100 <missa4312@gmail.com>
2025-08-12 22:43:38 +0300

    add Idea struct

--- src/FundVerse_backend/src/lib.rs
+++ src/FundVerse_backend/src/lib.rs
@@ -1,4 +1,23 @@
+// idea trait 
+
+struct Idea {
+    title: String,
+    description: String,
+    funding_goal: u64,
+    current_funding: u64,
+    legal_entity: String,
+    status: String, // e.g., "open", "funded", "closed"
+    contact_info: String,
+    category: String, // e.g., "technology", "healthcare", "education"
+    business_registration:u8,
+    created_at: u64, // timestamp
+    updated_at: u64, // timestamp
+}
+
+
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
